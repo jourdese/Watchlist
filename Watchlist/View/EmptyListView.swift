52fb132d0e0608bb208384f8a5820941132515b5
Jourdese Palacio <jourdesepalacio@gmail.com>
2025-09-02 15:34:20 +0800

    Empty List View (dark/light preview)

---
+++ Watchlist/View/EmptyListView.swift
@@ -0,0 +1,37 @@
+//
+//  EmptyListView.swift
+//  Watchlist
+//
+//  Created by Jourdese Palacio on 9/2/25.
+//
+
+import SwiftUI
+
+struct EmptyListView: View {
+    var body: some View {
+        Circle()
+            .fill(.blue.gradient)
+            .stroke(.blue.gradient.opacity(0.15), lineWidth: 30)
+            .stroke(.blue.gradient.opacity(0.15), lineWidth: 60)
+            .stroke(.blue.gradient.opacity(0.15), lineWidth: 90)
+            .frame(width: 180)
+            .overlay {
+                Image(systemName: "movieclapper")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100)
+                    .foregroundStyle(Color(UIColor.secondarySystemBackground))
+                    .padding(.top, -6)
+                
+            }
+    }
+}
+
+#Preview ("Light Theme"){
+    EmptyListView()
+}
+
+#Preview ("Dark Theme"){
+    EmptyListView()
+        .preferredColorScheme(.dark)
+}
