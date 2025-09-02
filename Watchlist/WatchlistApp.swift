c336d904b700a5bef7e86191913c686cb1f6474f
Jourdese Palacio <jourdesepalacio@gmail.com>
2025-09-02 12:12:10 +0800

    Initial Commit

---
+++ Watchlist/WatchlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchlistApp.swift
+//  Watchlist
+//
+//  Created by Jourdese Palacio on 9/2/25.
+//
+
+import SwiftUI
+
+@main
+struct WatchlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
